066e5e4fee6a9c7efae67ba1f4d6333d1a6e10e6
Jihyun Lim <55045377+jadeneu@users.noreply.github.com>
2021-09-27 07:53:31 +0900

    Feat: Question, Comment, Hall-of-Fame 모델 추가 (#5)
    * Update tests.py
    
    * Feat: Question, Comment, HoF 모델 추가

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,16 @@
+
 from django.db import models
+
+
+class Question(models.Model):
+    pass
+
+class Comment(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    comment = models.TextField()
+    create_date = models.DateTimeField()
+
+class Hall_of_Fame(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("안녕하세요 ama에 오신것을 환영합니다.")
+
+def post(request):
+    return HttpResponse("post")
+
+def hall_of_fame(request):
+    return HttpResponse("hall-of-fame")
