8b8b5c8bad2e90710a165df7e7c1ea101b716955
menmenmeng <75026772+menmenmeng@users.noreply.github.com>
2021-09-28 14:42:45 +0900

    Question 조회, 생성

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('', views.index),
     path('post/', views.post),
     path('hall-of-fame/', views.hall_of_fame),
+
+    path('post/<int:question_id>/', views.question_display),
+    path('post/question/', views.question_create),
 ]
 
 #test
