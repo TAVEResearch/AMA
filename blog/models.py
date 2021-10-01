138d556c6d7d8fbe8eeb10064254905a4cb6d838
menmenmeng <75026772+menmenmeng@users.noreply.github.com>
2021-10-01 22:20:28 +0900

    blank True 추가

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Question(models.Model):
     subject = models.CharField(max_length=200)
     vs_or_not = models.BooleanField(default=True)
-    side_1 = models.CharField(max_length=200)
-    side_2 = models.CharField(max_length=200)
+    side_1 = models.CharField(max_length=200, blank=True)
+    side_2 = models.CharField(max_length=200, blank=True)
     create_date = models.DateTimeField()
     like = models.IntegerField()
     dislike = models.IntegerField()
